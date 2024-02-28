5b6b0aa02125abe2c3f5abc38e0b5d9ef3ed7375
mativ707 <matias.k.velazquez@gmail.com>
2024-02-28 13:03:55 -0500

    primer commit

---
+++ Peluqueria/src/main/java/com/mativ707/Peluqueria/PeluqueriaApplication.java
@@ -0,0 +1,13 @@
+package com.mativ707.Peluqueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluqueriaApplication.class, args);
+	}
+
+}
